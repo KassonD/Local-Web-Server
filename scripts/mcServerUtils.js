5c5b33afbd8735a254956bd917c741eedc68eb0b
Kasson Davis <135732515+KassonD@users.noreply.github.com>
2025-09-21 14:57:33 -0500

    Uploading Project

---
+++ scripts/mcServerUtils.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+const path = require('path')
+
+function startServer(serverName) {
+    const serverDirectory = path.join('servers', serverName)
+    const bat = exec('run.bat', {cwd: serverDirectory});
+
+    bat.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+    })
+
+    bat.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+    })
+
+    return bat
+}
+
+function stopServer(serverProcess) {
+    serverProcess.stdin.write('/stop\n')
+}
+
+module.exports = {
+    startServer,
+    stopServer
+}
---
+++ scripts/serverPage.js
@@ -0,0 +1,43 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        document.querySelector(tab.dataset.tabTarget).classList.add('active')
+    })
+})
+
+function startServer() {
+    fetch('/server/start', {method: 'POST'})
+        .catch(error => console.error(error))
+
+    document.getElementById('button_start').disabled = true
+    document.getElementById('button_stop').disabled = false
+    document.getElementById('output').innerHTML = "Starting..."
+}
+
+function stopServer() {
+    fetch('/server/stop', {method: 'POST'})
+        .catch(error => console.error(error))
+
+    document.getElementById('button_start').disabled = false
+    document.getElementById('button_stop').disabled = true
+    document.getElementById('output').innerHTML = "Stopping..."
+}
+
+function enableServerButtons() {
+    console.log('serverProcessgffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+    // if (serverProcess == null) {
+    //     document.getElementById('button_start').disabled = true
+    // }
+    // else {
+    //     document.getElementById('button_stop').disabled = true
+    // }
+}
